31be312eb6be3cdd0b44fd8e8b1f4825aea51c0c
BARS-2080 <75639270+Max-2080@users.noreply.github.com>
2026-03-01 23:48:21 +0300

    Create MainWindow.xaml.cs

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+// Views/MainWindow.xaml.cs
+using System.Windows;
+
+namespace LibraryManagement.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
