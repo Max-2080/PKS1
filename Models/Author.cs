ee08379f34edaa916db6d086f9679206ce619421
BARS-2080 <75639270+Max-2080@users.noreply.github.com>
2026-03-01 23:34:55 +0300

    Create Author.cs

---
+++ Models/Author.cs
@@ -0,0 +1,30 @@
+// Models/Author.cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+        
+        [MaxLength(100)]
+        public string Country { get; set; }
+        
+        // Навигационное свойство
+        public virtual ICollection<Book> Books { get; set; }
+        
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
